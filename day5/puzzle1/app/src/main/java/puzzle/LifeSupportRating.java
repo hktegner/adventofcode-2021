40e51072d77e5bdb302b7fd2aed0f1ac518dccdf
Henning Tegner <henning.tegner@gmail.com>
2021-12-19 12:49:08 +0000

    Day 5 puzzle 1 + 2 solutions

--- day5/puzzle1/app/src/main/java/puzzle/LifeSupportRating.java
+++
@@ -1,9 +0,0 @@
-package puzzle;
-
-public record LifeSupportRating(Bits o2GenRating, Bits co2ScrubRating) {
-
-    public int value() {
-        return o2GenRating.value() * co2ScrubRating.value();
-    }
-
-}
